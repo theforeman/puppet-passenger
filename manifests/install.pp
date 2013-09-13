712f5b64a55b50777cf85ef7a70e8a18f7a5b760
Ewoud Kohl van Wijngaarden <e.kohlvanwijngaarden@oxilion.nl>
2013-09-13 15:33:04 +0200

    Do not install the repo by default
    Since Fedora 17 is no longer supported and for EL5 foreman has
    mod_passenger packaged, installing the standard repo is no longer
    needed.
    
    The class is still available in case people want to use the module
    without foreman.

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,13 +1,28 @@
 class passenger::install {
   case $::osfamily {
     RedHat: {
-      include passenger::install::redhat
+      $package_name = 'mod_passenger'
     }
     Debian: {
-      include passenger::install::debian
+      $package_name = 'libapache2-mod-passenger'
+
+      # Enable RequestHeaders
+      exec { 'enable-headers':
+        command => '/usr/sbin/a2enmod headers',
+        creates => '/etc/apache2/mods-enabled/headers.load',
+        require => Class['apache::install'],
+        notify  => Class['apache::service'],
+      }
     }
     default: {
-      fail("${::hostname}: This module does not support operatingsystem ${::osfamily}")
+      fail("${::hostname}: This module does not support osfamily ${::osfamily}")
     }
+  }
+
+  package { 'passenger':
+    ensure  => installed,
+    name    => $package_name,
+    require => Class['apache::install'],
+    before  => Class['apache::service'],
   }
 }
